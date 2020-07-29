785614fdc00846bde5cb55dababc100f1d8dd0dd
Stephen Plaza <plazas@janelia.hhmi.org>
2020-07-29 17:45:42 -0400

    updates creator script

--- scripts/create_composer_env.sh
+++ scripts/create_composer_env.sh
@@ -8,13 +8,13 @@ gcloud composer environments create emprocess --location us-east4 \
 	--zone us-east4-c \
 	--machine-type n1-standard-2 \
 	--image-version composer-1.10.0-airflow-1.10.6 \
-	--airflow-configs=celery-worker_concurrency=12,scheduler-max_threads=4 \
+	--airflow-configs=celery-worker_concurrency=12,scheduler-max_threads=4,core-dag_concurrency=128,core-parallelism=128 \
 	--env-variables=emproccess_version=0.1 \
 	--python-version 3
 
 # set http_requests pool to at least 32
 gcloud composer environments run emprocess --location us-east4 \
-	pool -- -s http_requests 64 httppool 
+	pool -- -s http_requests 128 httppool 
 
 # set connectionsn ALIGN_CLOUD_RUN and IMG_WRITE
 gcloud composer environments run emprocess --location us-east4 \
